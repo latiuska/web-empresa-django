c8771369b42c6b85271ce1c0a430db7e81803b86
latiuska <latiprogramacion@gmail.com>
2024-05-31 16:34:42 +0200

    Update urls.py
    Actualización para que en el panel de administrador aparezca el nombre de la empresa, en vez de Panel de Administrador

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -50,3 +50,8 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#Custom tites for admin
+admin.site.site_header = "La Caffetiera"
+admin.site.index_title = "Panel de administrador"
+admin.site.site_title = "La Caffetiera"
